0bf09ba01d0508f15c4bc3fe62ebd49fe2b805f3
DannyLiehr <dannyliehr@gmail.com>
2023-08-10 17:56:46 -0400

    Cleaned up a bit. Added pseudocode plan for saving files.

--- jsx/exec_photoshop.jsx
+++ jsx/exec_photoshop.jsx
@@ -1,5 +1,4 @@
 //Code you want to execute
-// app.doAction("ActionStep","ActionFile.ATN");
 var act = argv1 + " (" + argv4 + ")";
 var actset;
 var fileSelect;
@@ -28,32 +27,40 @@ var file = new File(fileStr);
 // Open the file for reading.
 // file.open("r");
 
-// app.activeDocument.activeLayer = app.activeDocument.artLayers.getByName("Player");
-// app.doAction(act, actionPath);
-alert("Opening Football image now.");
 app.open(file);
 // If the image selected is the back version, set text.
 if (argv4=="Helmet"){
     // Helmet
-    app.activeDocument.activeLayer = app.activeDocument.artLayers.getByName("Shell");  
-
-} else{
-   if (argv4 == "Back"){
-    // Has text.
-    // alert("Applying back jersey text: " + argv3);
-    app.activeDocument.activeLayer = app.activeDocument.artLayers.getByName("- EDIT TEXT - TEAM");
-    targetLayer = app.activeDocument.artLayers.getByName("- EDIT TEXT - NUMBER");
-    targetLayer.textItem.contents = argv3;
-
+    app.activeDocument.activeLayer = app.activeDocument.artLayers.getByName("Shell");
 } else {
-    // Front Facting player
-    app.activeDocument.activeLayer = app.activeDocument.artLayers.getByName("Player");  
-    targetLayer = app.activeDocument.artLayers.getByName("- EDIT TEXT -");
-    targetLayer.textItem.contents = argv7;
-} 
+
+   if (argv4 == "Back"){
+        // Has text.
+        app.activeDocument.activeLayer = app.activeDocument.artLayers.getByName("- EDIT TEXT - TEAM");
+        targetLayer = app.activeDocument.artLayers.getByName("- EDIT TEXT - NUMBER");
+        targetLayer.textItem.contents = argv3;
+    } else {
+        // Front Facting player
+        app.activeDocument.activeLayer = app.activeDocument.artLayers.getByName("Player");  
+        targetLayer = app.activeDocument.artLayers.getByName("- EDIT TEXT -");
+        targetLayer.textItem.contents = argv7;
+    } 
 
 }
 
-// app.activeDocument.activeLayer = app.activeDocument.artLayers.getByName("- EDIT TEXT - NUMBER");
-
 app.doAction(act, actset);
+
+/* Pseudocode/Bard Generated Code:
+
+Note to self: Use PSD and not JPG.
+
+// Create a JPEGSaveOptions object
+var jpgSaveOptions = new JPEGSaveOptions()
+
+// Set the quality of the JPEG image
+jpgSaveOptions.quality = 80
+
+// Save the document as a JPEG file
+doc.saveAs(saveFile, jpgSaveOptions)
+
+*/
